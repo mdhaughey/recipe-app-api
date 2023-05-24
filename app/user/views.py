6714989442631c622181362d98f50c73533e0948
mdhaughey <mhaughey86@gmail.com>
2023-05-24 19:57:21 -0400

    User Auth Added

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,11 @@
 """Views forthe user API"""
 from rest_framework import generics  # type: ignore
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken  # type: ignore
+from rest_framework.settings import api_settings  # type: ignore
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 # Create your views here.
@@ -8,3 +13,10 @@ class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
 
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+
+    serializer_class = AuthTokenSerializer
+    render_classes = api_settings.DEFAULT_RENDERER_CLASSES
